b40c3581c4d317ac0639e8acf50e5fbd0c40eee8
MarWalldorf <mw305420@student.polsl.pl>
2022-12-01 17:31:19 +0100

    commit people class

--- app/People/Person.php
+++ app/People/Person.php
@@ -5,7 +5,15 @@ namespace App\People;
 class Person
 {
 
-    public static function findOrFail(int $id)
+    public static function setFullName($name, $surname)
     {
-    }
+        $this->name = $name;
+        $this->surname = $surname;
+    } // end setFullName();
+
+    public function getFullName()
+    {
+        return $this->name.' '.$this->surname;
+    } // end getFullName();
+
 }
