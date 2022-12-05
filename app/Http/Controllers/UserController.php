89a9fdc0f35cb0c76b7d79daceab12d06c21af8c
MarWalldorf <mw305420@student.polsl.pl>
2022-12-05 18:27:49 +0100

    initial commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,7 +1,26 @@
 <?php
+declare (strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\UserController;
 use App\People\Person;
-use Illuminate\Support\Facades\Route;
+use http\Env\response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Js;
+
+class UserController extends Controller
+{
+    public function index():JsonResponse
+    {
+        return response::json(Person::all(), 200);
+    }
+
+
+    public function show(Person$person):JsonResponse
+    {
+       return response ()json($person,200);
+
+    }
+}
 
