8fe7fb6181de5ab977eee619e9e526accf8107b2
MarWalldorf <mw305420@student.polsl.pl>
2022-12-15 12:47:49 +0100

    initial commit

--- database/seeders/PeopleSeeder.php
+++ database/seeders/PeopleSeeder.php
@@ -7,5 +7,20 @@ use Faker\factory;
 use Illuminate\Support\Facades\DB;
 class PeopleSeeder extends Seeder
 {
-    
+    public function run():void
+    {
+        DB::table('people')->delete();
+        $faker=Factory::create();
+        for($i=0;$i<50;$i++) {
+            People::create
+            ([
+                'name' => $faker->firstName(),
+                'surname' => $faker->lastName(),
+                'phone_num' => $faker->e164PhoneNumber(),
+                'street' => $faker->streetAddress(),
+
+
+            ]);
+        }
+    }
 }
