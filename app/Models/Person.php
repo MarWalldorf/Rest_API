0953ea7f669d685dec39566fbe2fd62166a47b5a
MarWalldorf <mw305420@student.polsl.pl>
2022-12-15 12:31:04 +0100

    initial commit

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -4,14 +4,18 @@ namespace App\Models;
 ;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 
 class Person extends Model
 {
-    protected $fillable=['title','body','author_id'];
+    protected $fillable =['firstname','lastname','person_id'];
 
     protected $table='People';
 
     use HasFactory;
+    public function articles():HasMany
+    {
+        return $this->hasmany('\Models\Person','person_id','id');
+    }
 }
---
+++ database/seeders/PeopleSeeder.php
@@ -0,0 +1,11 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\People;
+use Faker\factory;
+use Illuminate\Support\Facades\DB;
+class PeopleSeeder extends Seeder
+{
+    
+}
