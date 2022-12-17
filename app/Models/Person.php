aff9867dbc261b1881fe235dc194ed1e5a3a8e7d
MarWalldorf <mw305420@student.polsl.pl>
2022-12-17 13:32:09 +0100

    controller update v4

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -1,21 +1,13 @@
 <?php
+
 namespace App\Models;
 
-;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-
 
 class Person extends Model
 {
-    protected $fillable =['firstname','lastname','person_id'];
-
-    protected $table='People';
-
+    protected $table = 'person';
+    protected $fillable = ['name', 'surname', 'phone', 'street', 'city','age'];
     use HasFactory;
-    public function articles():HasMany
-    {
-        return $this->hasmany('\Models\Person','person_id','id');
-    }
 }
